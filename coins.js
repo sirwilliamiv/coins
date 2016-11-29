5e49608828a9758819af528eaa22a3bee4801878
sirwilliamiv <wjconnolly2@gmail.com>
2016-11-29 13:34:23 -0600

    completed coins exercise

--- coins.js
+++ coins.js
@@ -7,16 +7,27 @@ function coinCounter (amount) {
   coinPurse.nickels = 0;
   coinPurse.pennies = 0;
   amount = amount * 100;
- 			
-  	coinPurse.quarters = parseInt(amount/25)
-  	console.log("quarter=", coinPurse.quarters)
-  	coinPurse.dimes = parseInt((amount% 25) /10)
-  	console.log("dimes=", coinPurse.dimes)
-  	coinPurse.nickels()
 
- 			parseInt(amount)
-  			console.log(amount)
-		amount = 0
+
+
+ 			// returns amount whole integer w/o decimal
+  	coinPurse.quarters = parseInt(amount/25)
+  	// console.log("quarter=", coinPurse.quarters)
+  	// 
+  	// divides remainder by 10
+  	coinPurse.dimes = parseInt((amount% 25) /10)
+  	// console.log("dimes=", coinPurse.dimes)
+  	// 
+  	// divides remainder by 5
+  	coinPurse.nickels = parseInt(((amount% 25)%10) /5)
+  	// console.log("nickels=", coinPurse.nickels)
+  	// 
+  	// divides remainder by 1 (redundant)
+  	coinPurse.pennies = parseInt((((amount% 25)%10)%5)/1)
+
+ 			
+  			// console.log("pennies =", coinPurse.pennies)
+		
 
 
   	
@@ -24,5 +35,5 @@ function coinCounter (amount) {
   return coinPurse;
 }
 
-var coins = coinCounter(.67)
+var coins = coinCounter(.88)
 console.log(coins);
