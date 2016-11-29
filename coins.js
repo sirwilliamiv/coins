95fee0689b1ba569424c6140b18d144e894ecbd8
sirwilliamiv <wjconnolly2@gmail.com>
2016-11-29 13:24:00 -0600

    quarters and dimes successful

--- coins.js
+++ coins.js
@@ -7,37 +7,19 @@ function coinCounter (amount) {
   coinPurse.nickels = 0;
   coinPurse.pennies = 0;
   amount = amount * 100;
-  // remainder
-  // something that loops through it till the amount goes to zero
-  // if else
-  // then clean up with switch
+ 			
+  	coinPurse.quarters = parseInt(amount/25)
+  	console.log("quarter=", coinPurse.quarters)
+  	coinPurse.dimes = parseInt((amount% 25) /10)
+  	console.log("dimes=", coinPurse.dimes)
+  	coinPurse.nickels()
 
-// if remainer is zero, it's safe to divide by .25
-  if (amount %25 === 0) {
-  	coinPurse.quarters = amount / 25;
-  	// amount = amount - (coinPurse.quarters)
-  	// console.log(amount);
-  } else 
-  		if (amount %25 > 0) {
-  			amount = amount - (amount/25);
-  			console.log(amount/100);
-  		}
-
-  // else {
-  // 		if (amount%.10 === 0) {
-  // 		coinPurse.dimes = amount / .10;
-		// } 
-
-		//   else {
-		//   	if (amount%.05 ===0) {
-  // 		coinPurse.dimes = amount / .05;
-  // 			}
-		// }
+ 			parseInt(amount)
+  			console.log(amount)
+		amount = 0
 
 
-
-  // 	}
-
+  	
 
   return coinPurse;
 }
